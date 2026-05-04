6c78fc7a9dca435127659268a267001ceaadbffb
AbdullahHAboElkeir <bedohisham234@gmail.com>
2026-05-04 09:52:14 +0300

    yeeeeeeeeeeeeeeeeeeeeeeeee
    yeeeeeeeeeeeeeeee

--- app/models/Comment.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/models/Comment.php
-
-require_once __DIR__ . '/../../core/Model.php';
-
-class Comment extends Model {
-    protected $table = 'comments';
-}
-?>
--- app/models/Role.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/models/Role.php
-
-require_once __DIR__ . '/../../core/Model.php';
-
-class Role extends Model {
-    protected $table = 'roles';
-}
-?>
